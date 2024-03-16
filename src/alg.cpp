aface4ba57b8232894e1303db241135b479f6138
alinaavv <160399232+alinaavv@users.noreply.github.com>
2024-03-16 11:18:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,33 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int count = 0;
+    int left = 0;
+    int right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        
+        if (arr[mid] == value) {
+            count++;
+            int leftCount = mid - 1;
+            while (arr[leftCount] == value && leftCount >= 0) {
+                count++;
+                leftCount--;
+            }
+            int rightCount = mid + 1;
+            while (arr[rightCount] == value && rightCount < size) {
+                count++;
+                rightCount++;
+            }
+            break;
+        } 
+        else if (arr[mid] < value) {
+            left = mid + 1;
+        } 
+        else {
+            right = mid - 1;
+        }
+      return count;
+    }  
+  return 0;
 }
